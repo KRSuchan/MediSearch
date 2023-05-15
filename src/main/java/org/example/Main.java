5661df5accf75311cc9a1055da412153775aeaae
sjsk3232 <sjsk3232@gmail.com>
2023-05-15 16:53:02 +0900

    23.05.15. MainScene Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
     public static void main(String[] args) {
         // API 패키지는 아직 미완성이니 사용은 하지 말아주세요
         System.out.println("test3 world!");
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainScene.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("Main");
+        stage.setScene(scene);
+        stage.show();
     }
 }
